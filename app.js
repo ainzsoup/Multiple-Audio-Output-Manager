0f4a68c1a4b69f5ebb8a776fee983ca4432a8091
ainzsoup <gamraouis@gmail.com>
2023-08-31 10:59:34 +0100

    need to fix switching audio when reading desctiption peoperty

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
-const shell = require(`shelljs`);
-const readline = require('readline');
+import shell from 'shelljs';
+import readline from 'readline';
+import { PulseAudio } from 'pulseaudio.js';
+import prompt from 'prompt-sync';
+import PromptSync from 'prompt-sync';
+
 const green = '\x1b[32m';
 const reset = '\x1b[0m';
 const red = '\x1b[31m';
 const yellow = '\x1b[33m';
 const blue = '\x1b[34m';
 
-async function combineSinks(selectedSinks, combinedSinkName) {
-    const combinedSinkIndex = shell.exec(`pactl load-module module-combine-sink sink_name=${combinedSinkName} slaves=${selectedSinks.join(',')}`, { silent: true }).stdout.trim();
-    console.log('Combined sink created with index:', combinedSinkIndex);
+async function combineSinks(devices) {
+	console.clear();
+    let slaves = devices.split(' ').join(',');
+	const prompt = PromptSync();
+	const description = prompt('Enter Name >');
 
-    // Set the combined sink as the default sink
-    shell.exec(`pactl set-default-sink ${combinedSinkIndex}`);
+    const pactlCommand = `pactl load-module module-combine-sink slaves="${slaves}" sink_properties="device.description=${description}" `;
+	console.log (pactlCommand);
 
-    console.log('Combined sink set as default.');
+    const result = shell.exec(pactlCommand);
+
+    if (result.code === 0) {
+        console.log('Comb created');
+    } else {
+        console.error('Error creating comb:', result.stderr);
+    }
 }
 
 function help() {
-	let list = `${blue}list: lists the available outputs\n`;
-	list +=    `switch: switch output device`;
+	console.clear();
+	let list = `list: lists the available outputs\n`;
+	list +=    `switch: switch output device\n`;
 	list +=    `comb: create a multiple output comb\n`;
-	list +=    `del: delete a comb${reset}`;
+	list +=    `del: delete a comb`;
 	console.log(list);
 }
 
-function listSinks(sinks) {
-	// console.clear();
+function listSinks(sinks, combs = true) {
+	console.clear();
 	console.log('------------------------------------------');
 	console.log(`${red}Available Devices:${reset}`);
 	for (let sink of sinks.devices)
-		console.log(sink.index, sink.name);
-	console.log(`${red}Available combs:${reset}`);
-	for (let sink of sinks.combs)
-			console.log(sink.index, sink.name);
+		console.log(sink.index, sink.description);
+	if (combs) {
+		console.log(`${red}Available combs:${reset}`);
+		for (let sink of sinks.combs)
+			console.log(sink.index, sink.description);
+	}
 	console.log('------------------------------------------');
 }
 
-function updateSinks() {
-	const sinksOutput = shell.exec(`pactl list sinks`, { silent: true }).stdout;
-	const sinksArray = sinksOutput.split('Sink #');
-	const sinks = sinksArray.slice(1).map(sink => {
-		const lines = sink.trim().split('\n');
-		const index = parseInt(lines[0]);
-		const name = lines.find(line => line.includes('Description:')).split(':')[1].trim();
-		const driver = lines.find(line => line.includes('Driver:')).split(':')[1].trim();
-		const ownerModule = parseInt(lines.find(line => line.includes('Owner Module:')).split(':')[1].trim());
-
-		return { index, name, driver, ownerModule};
-	});
+async function updateSinks(pa) {
+	let sinks = await pa.getAllSinks();
 	let devices = [];
 	let combs = [];
 	for (let sink of sinks) {
@@ -65,28 +70,42 @@ async function unloadModule(moduleIndex) {
 }
 
 function listCombs(sinks) {
+	console.clear();
 	if (sinks.combs.length === 0) {
 		console.log('no available combs.');
 		return false;
 	}
 	console.log(`${red}Pick the index of the comb to delete:${reset}`);
 	for (let sink of sinks.combs) {
-		console.log(sink.ownerModule, sink.name);
+		console.log(sink.module, sink.description);
 	}
 	return true;
 }
 
+async function switchOutput (pa, sinks) {
+	listSinks(sinks);
+	const prompt = PromptSync();
+	const output = prompt('Enter index of output to switch to >');
+	await pa.setDefaultSink(output);
+	console.log('Output switched to:', sinks.devices.find(sink => sink.index === parseInt(output)).description);
+}
+
 async function main () {
+	const pa = new PulseAudio();
+	await pa.connect();
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 	console.log('Welcome to scuffed Audio-manager v1.0.\nType "help" for more information.')
     const promptUser = async () => {
-		let sinks = updateSinks();
+		let sinks = await updateSinks(pa);
         rl.question(`> `, async (command) => {
             switch (command) {
 				case '':
+					break;
+				case 'clear':
+					console.clear();
 					break;
                 case 'help':
                     help();
@@ -94,6 +113,17 @@ async function main () {
                 case 'list':
 					listSinks(sinks);
                     break;
+				case 'comb':
+					if (sinks.devices.length < 2)
+						console.log('Not enough devices to combine.');
+					else
+						listSinks(sinks, false)
+						rl.question('type indexes of devices to comb space sparated\n> ', async (devices) => {
+							await combineSinks(devices);
+							promptUser();
+						});
+						
+					break;
 				case 'del':
 					if (listCombs(sinks)) {
 						rl.question('> ', async (number) => {
@@ -103,6 +133,9 @@ async function main () {
 						});
 						return;
 					}
+					break;
+				case 'switch':
+						await switchOutput(pa, sinks);
 					break;
                 default:
                     console.log('Unknown command. Type "help" for available commands.');
